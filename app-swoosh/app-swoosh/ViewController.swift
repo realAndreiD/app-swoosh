f3d77bef3277154bf9deee7e6d18320b07ac3bfc
Mordre <mordre@Macintosh.local>
2017-09-21 15:31:49 +0300

    New features

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Mordre on 9/21/17.
+//  Copyright © 2017 Andrei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
